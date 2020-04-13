4ce52c0115cf73e06b122a182f695631aa87b60e
Korakot Santiudommongkol <korakotsanti@gmail.com>
2020-04-13 16:42:59 -0400

    s

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "",
-    USER: "",
-    PASSWORD: "",
-    DB: ""
+    HOST: "gamedb.cidt3xdosoj7.us-east-2.rds.amazonaws.com",
+    USER: "team",
+    PASSWORD: "teamworkmakesthedreamwork",
+    DB: "game_database"
 };
--- route/game.js
+++ route/game.js
@@ -30,4 +30,40 @@ router.post("/game", (req,res) => {
     res.redirect("/game");
 });
 
+router.get("/game/:id", (req,res) => {
+    result = sql.query(`select TheView.game_id, Game, Developer, Publisher, ifnull(Quality,'N/A') as Quality, ifnull(Maturity, 'N/A') as Maturity, genre_name, platform_name from TheView 
+    left outer join game_genre on TheView.game_id=game_genre.game_id
+    left outer join genre on game_genre.genre_id=genre.genre_id
+    left outer join game_platform on TheView.game_id=game_platform.game_id
+    left outer join platform on game_platform.platform_id=platform.platform_id
+    where TheView.game_id=${req.params.id};`, (err, rows, fields) => {
+        if(!err){
+            let gamedata={
+                "gamename":rows[0]['Game'], 
+                "Developer":rows[0]['Developer'], 
+                "Publisher":rows[0]['Publisher'], 
+                "Quality": rows[0]['Quality'],
+                "Maturity": rows[0]['Maturity']
+            }
+
+            let genres = new Set()
+            let plat = new Set()
+
+            for(var i=0; i < rows.length; i++) {
+                genres.add(rows[i]['genre_name']);
+                plat.add(rows[i]['platform_name']);
+            }
+
+            gamedata['genres'] = Array.from(genres);
+            gamedata['platforms'] = Array.from(plat);
+
+            console.log(gamedata);
+            res.render("showgame", {game:gamedata});
+        } else {
+            console.log(err);
+            res.redirect("/game");
+        }
+    });
+});
+
 module.exports = router;
