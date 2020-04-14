12268eb80612bbfa099d5a54b24f5ad6d90bcaa2
Korakot Santiudommongkol <47130579+KorakotSanti@users.noreply.github.com>
2020-04-14 13:42:50 -0400

    Update config.py

--- game_data/scripts/config/config.py
+++ game_data/scripts/config/config.py
@@ -1,7 +1,7 @@
 """
 Config file that stores information to access local database
 """
-username = "admin"
-password = "2x!MrJ35UBgbhvDP"
-host = "gamedb.cidt3xdosoj7.us-east-2.rds.amazonaws.com"
+username = ""
+password = ""
+host = ""
 db = 'game_database'
