7458e15fd349f7ff004aff8b43d33ec734994b03
Korakot Santiudommongkol <korakotsanti@gmail.com>
2020-04-20 13:17:07 -0400

    added more index

--- game_data/sql/index.sql
+++ game_data/sql/index.sql
@@ -14,3 +14,18 @@ on genre(genre_name);
 
 create index platform_name
 on platform(platform_name);
+
+create index pub_id
+on publisher(pub_id);
+
+create index dev_id
+on developer(dev_id);
+
+create index game_id
+on game(game_id);
+
+create index genre_id
+on genre(genre_id);
+
+create index platform_id
+on platform(platform_id);
